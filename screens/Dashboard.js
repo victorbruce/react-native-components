6236121a4382e27c4af6c17f3b070c1f5e9a9eeb
victorbruce <victorbruce82@gmail.com>
2020-03-31 18:18:20 +0000

    Screens

---
+++ screens/Dashboard.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Block, Text } from "expo-ui-kit";
+
+const Dashboard = () => {
+  return (
+    <Block center middle>
+      <Text>Dashboard</Text>
+    </Block>
+  );
+};
+
+export default Dashboard;
---
+++ screens/Messages.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Block, Text } from "expo-ui-kit";
+
+
+const Messages = () => {
+  return (
+    <Block center middle>
+    <Text>Messages</Text>
+  </Block>
+  )
+}
+
+export default Messages;
